1109de336abecfee4d6c2ea1b4d1274ace1a5fd2
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-01-12 22:18:48 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+        char [] Temp=new char[s.length];
+        int num= s.length;
+        for(int x=0;x<s.length;x++){
+            
+            Temp[x]=s[num-x-1];
+        }
+       for(int y=0;y<num;y++){
+            s[y]=Temp[y];
+        }
+    }
+}
