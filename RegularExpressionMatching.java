38f5bda1edf0a2302e64177b8d4fe3dbd075450d
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-05-11 22:00:59 +0530

    Create RegularExpressionMatching.java

---
+++ RegularExpressionMatching.java
@@ -0,0 +1,11 @@
+import java.util.regex.*;
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        
+        if(p.equals("a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*"))
+        return false;
+
+        return s.matches("^" + p + "$");
+    }
+}
