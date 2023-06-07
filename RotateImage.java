a526ee786d805a932d5891fcc0ee93350b2c812c
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-06-07 20:12:18 +0530

    Update RotateImage.java

--- RotateImage.java
+++ RotateImage.java
@@ -1,28 +1,17 @@
-class Solution {
+cclass Solution {
     public void rotate(int[][] matrix) {
-        
-        
        int m = matrix.length;
-        int n = matrix[0].length;
-        
-        for(int i=1;i<m;i++){
-            for(int j=0;j<i;j++){
-                
-                matrix[i][j]=matrix[i][j]-matrix[j][i];
-                matrix[j][i]=matrix[i][j]+matrix[j][i];
-                matrix[i][j]= matrix[j][i]-matrix[i][j];
+       int n = matrix[0].length;
+       int[][] a=new int[m][n];
+       for(int j=0,k=0;j<n;j++,k++){
+           for(int i=m-1,l=0;i>=0;i--,l++){
+               a[k][l]=matrix[i][j];
+           }
+       }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]=a[i][j];
             }
         }
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n/2;j++){
-                
-                matrix[i][j]=matrix[i][j]-matrix[i][n-j-1];
-                matrix[i][n-j-1]=matrix[i][j]+matrix[i][n-j-1];
-                matrix[i][j]= matrix[i][n-j-1]-matrix[i][j];
-            }
-                    
-        
-    }
     }
 }
