83060c114eff2669a89795e9a87357ef2e6421b9
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-09-28 22:30:22 +0530

    Create Sort_Array_By_Parity.java

---
+++ Sort_Array_By_Parity.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int[] temp = new int[nums.length];
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                temp[k++] = nums[i];
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2!=0){
+                temp[k++] = nums[i];
+            }
+        }
+        return temp;
+    }
+}
