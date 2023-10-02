f36b81bfe9784f68172a71eab3224ab3cfe9d68d
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-10-02 22:25:12 +0530

    Time: 1 ms (71.83%), Space: 41.2 MB (88.91%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(String s:operations){
+            if(s.charAt(0)=='-'|| s.charAt(1)=='-')
+                x-=1;
+             if(s.charAt(0)=='+'|| s.charAt(1)=='+')
+                x+=1;
+            
+        }
+        return x;
+    }
+}
