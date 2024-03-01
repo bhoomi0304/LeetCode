58ff011db9e72b737d26db9332d766f09fcc932e
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2024-03-01 21:47:01 +0530

    Update Two_Sum.java

--- Two_Sum.java
+++ Two_Sum.java
@@ -2,7 +2,7 @@
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int x[]=new int[3];
+        int x[]=new int[2];
         int k=0;
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
